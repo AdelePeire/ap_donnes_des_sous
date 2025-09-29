e62695255a40df3d419cd98519535f396bd9afcb
Adele Peire <adele.peire@ig2i.centralelille.fr>
2025-09-29 16:01:07 +0200

    first commit

---
+++ tests/test_banque.py
@@ -0,0 +1,9 @@
+from ap_donnes_des_sous.banque import ap_donnes_des_sous
+
+def test_donnes_des_sous(): 
+    result = ap_donnes_des_sous(12, 11)
+    assert result == 23
+
+def test_donnes_beaucoup_des_sous():
+    result = ap_donnes_des_sous(212, 11)
+    assert result == 23
