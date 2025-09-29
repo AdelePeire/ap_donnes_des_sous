9add087344538d5e69c93d3c8a18100555801260
Adele Peire <adele.peire@ig2i.centralelille.fr>
2025-09-29 16:45:06 +0200

    test_banque

--- tests/test_banque.py
+++ tests/test_banque.py
@@ -1,4 +1,4 @@
-from src.ap_donnes_des_sous.banque import ap_donnes_des_sous
+from ap_donnes_des_sous.banque import ap_donnes_des_sous
 
 def test_donnes_des_sous(): 
     result = ap_donnes_des_sous(12, 11)
